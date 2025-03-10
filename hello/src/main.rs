335b1c90fbe3f48c90b46f9b0d03da80df6beadd
Perlou <perloukevin@gmail.com>
2025-03-10 20:12:55 +0800

    feat async

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,39 +1,47 @@
-struct Droppable {
-    name: &'static str,
-}
+use std::collections::HashMap;
 
-// 这个简单的 `drop` 实现添加了打印到控制台的功能。
-impl Drop for Droppable {
-    fn drop(&mut self) {
-        println!("> Dropping {}", self.name);
+fn call(number: &str) -> &str {
+    match number {
+        "798-1364" => {
+            "We're sorry, the call cannot be completed as dialed. 
+            Please hang up and try again."
+        }
+        "645-7689" => {
+            "Hello, this is Mr. Awesome's Pizza. My name is Fred.
+            What can I get for you today?"
+        }
+        _ => "Hi! Who is this again?",
     }
 }
 
 fn main() {
-    let _a = Droppable { name: "a" };
+    let mut contacts = HashMap::new();
 
-    // 代码块 A
-    {
-        let _b = Droppable { name: "b" };
+    contacts.insert("Daniel", "798-1364");
+    contacts.insert("Ashley", "645-7689");
+    contacts.insert("Katie", "435-8291");
+    contacts.insert("Robert", "956-1745");
 
-        // 代码块 B
-        {
-            let _c = Droppable { name: "c" };
-            let _d = Droppable { name: "d" };
-
-            println!("Exiting block B");
-        }
-        println!("Just exited block B");
-
-        println!("Exiting block A");
+    // 接受一个引用并返回 Option<&V>
+    match contacts.get(&"Daniel") {
+        Some(&number) => println!("Calling Daniel: {}", call(number)),
+        _ => println!("Don't have Daniel's number."),
     }
-    println!("Just exited block A");
 
-    // 变量可以手动使用 `drop` 函数来销毁。
-    drop(_a);
-    // 试一试 ^ 将此行注释掉。
+    // 如果被插入的值为新内容，那么 `HashMap::insert()` 返回 `None`，
+    // 否则返回 `Some(value)`
+    contacts.insert("Daniel", "164-6743");
 
-    println!("end of the main function");
+    match contacts.get(&"Ashley") {
+        Some(&number) => println!("Calling Ashley: {}", call(number)),
+        _ => println!("Don't have Ashley's number."),
+    }
 
-    // `_a` *不会*在这里再次销毁，因为它已经被（手动）销毁。
+    contacts.remove(&("Ashley"));
+
+    // `HashMap::iter()` 返回一个迭代器，该迭代器以任意顺序举出
+    // (&'a key, &'a value) 对。
+    for (contact, &number) in contacts.iter() {
+        println!("Calling {}: {}", contact, call(number));
+    }
 }
